7f967810c5a265b06d8569dc65f04cba04a26589
eanton71 <eanton71@gmail.com>
2024-01-28 20:28:23 +0100

    rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "eantondev.netlify.app",
   integrations: [preact()]
 });
